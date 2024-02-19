a18e0cf31d5a3ad16b4e89af11c0cccbf6993251
Josue Orantes <daniel02josue@gmail.com>
2024-02-19 16:45:09 -0600

    :sparkles:feat(perfilhackathon):agrego seccion de perfil hackathon

---
+++ src/components/PerfilHackathon/PerfilHackathon.jsx
@@ -0,0 +1,45 @@
+
+
+export const PerfilHackathon = () => {
+  return (
+    <>
+        <section name="PerfilHackathon">
+            <div className="container">
+                <div>
+                    <p>
+                        Tienes entre 18 y 29 años, 
+                        <span className="text-semibold">eres estudiante, egresado o graduado de universidades de el salvador</span>,
+                        acépta el desafío y tú con tú equipo podrán <span className="text-semibold">ganar hasta $3000 dólares</span>.
+                    </p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className="container">
+                <div>
+                    <span>El reto:</span>
+                    <p>
+                        Diseñar la oferta de valor y experiencia para los jóvenes universitarios que fortalezca el ecosistema digital
+                        de pagos en la universidades.
+                    </p>
+                </div>
+                <div>
+                    <span>¿Quiénes participan?</span>
+                    <p>
+                        Equipos de 2 a 5 personas conformados por estudiantes, egresados y graduados de Universidades de El Salvador, 
+                        de cualquier carrera afín a tecnología, administración de empresas y diseño
+                    </p>
+                </div>
+                <div>
+                    <span>¿Qué se debe entregar?</span>
+                    <p>
+                        Durante el evento de la Hackathon los equipos deben conceptualizar, prototipar a nivel de mockups y desarrollar
+                        la nueva funcionalidad y su código de desarrollo.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
