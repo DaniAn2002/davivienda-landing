1586123780a6603a1700185a8d4888d24d677546
Daniel An <cruzalexd1@gmail.com>
2024-02-19 10:02:41 -0600

    :bento:feat(app):add content to tailwind config file

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ function App() {
   return (
     <>
       < Navbar />
-      < Footer />
       < Premios />
+      < Footer />
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
-import { Flowbite } from 'flowbite-react'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/App.jsx",
-    "./src/components/Navbar.jsx"
+    "./src/components/Navbar.jsx",
+    "./src/components/Footer.jsx"
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [Flowbite],
+  plugins: [],
   
 }
 
