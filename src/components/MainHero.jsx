e25021a962808813ebb62c82d29708afe925c41b
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2024-02-21 12:39:48 -0600

    modificando componente register

--- src/components/MainHero.jsx
+++ src/components/MainHero.jsx
@@ -1,8 +1,8 @@
 const MainHero = () => {
   return (
-    <section className="flex justify-around relative max-w-[1280px] mx-auto items-center">
-      <div className="max-w-[500px]">
-        <h1 className="text-8xl block text-[#ED1C27] tracking-wide font-bold mb-4">
+    <section className="flex flex-row justify-around mx-auto items-center hero-content">
+      <div className="p-10">
+        <h1 className="text-8xl block text-[#ED1C27] tracking-wide font-bold mb-4 text-hero">
           Primer Hackatón
         </h1>
         <p className="text-4xl mb-2">Innovación abierta con jóvenes universitarios en <span className="text-blue-800">El Salvador</span></p>
@@ -10,7 +10,7 @@ const MainHero = () => {
         <a href="#" className="block rounded-3xl text-white font-bold bg-red-600 p-5 text-2xl mb-5 w-fit hover:shadow-xl hover:shadow-slate-950/40">Inscribase aquí</a>
       </div>
       <img src="https://res.cloudinary.com/dk2oxzxoo/image/upload/v1708385914/kodigo-Davivienda/hero-clean_jtgyno.png"
-        alt="hero-img" className="max-w-[500px] rounded-[10%]"/>
+        alt="hero-img" className="rounded-[10%] hero-img"/>
     </section>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,8 @@
 const Register = () => {
     return <>
-        <section className="m-5 text-center p-10 tracking-wider">
-            <button className="rounded-full text-white font-bold bg-red-600 p-5 text-2xl m-5">Inscribase y participe</button>
+        <section className=" text-center p-10 tracking-wider">
+            
+            <button className="rounded-full text-white font-bold bg-red-600 p-5 text-2xl m-5">Inscribase y participe <img src="https://res.cloudinary.com/dyhftwfrw/image/upload/v1708540321/davi/lnnauduleneuihmbrqjd.svg" alt="Inscribase y participe" className="img-button"/></button>
             <p className="text-center text-black text-2xl">
                 Mientras tanto, <span className="font-bold">vaya por las fotocopias o las onces, </span><span className=" text-red-600 italic font-bold underline underline-offset-8">descangando su DaviPlata</span>
             </p>
