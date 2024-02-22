51be5ff7ae87a26832411bb3d00d2e117dc6bfd2
Rudy Andres Rivera Aviles <RUDY.ANDRES.RIVERA@GMAIL.COM>
2024-02-21 20:38:28 -0600

    Arreglos menores en seccion de premios

--- src/components/Premios.jsx
+++ src/components/Premios.jsx
@@ -9,15 +9,15 @@ export const Premios = () => {
             <img src="https://res.cloudinary.com/dgxmqoyv4/image/upload/v1708554227/Davivienda/computer-program-coding-screen_wrk4lh.jpg" alt="Hero Image" className="md:angled-mask drop-shadow-lg" />
         </div>
         <div className=" md:w-1/2 mb-10 md:mb-0">
-            <h2 className="text-4xl font-bold leading-tight mb-4 text-center drop-shadow-lg pt-10">Tres dias de hacking intenso!</h2>
+            <h2 className="text-4xl font-bold leading-tight mb-4 text-center drop-shadow-lg pt-10">Tres días de hacking intenso!</h2>
             <div className="flex flex-wrap flex-row justify-center items-center">
-            <h1 className="rounded-full font-bold text-6xl text-white text-center  flex items-center justify-center w-32 h-32 p-10 bg-white border border-gray-200 drop-shadow-lg hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700 mx-auto my-4">
+            <h1 className="rounded font-bold text-6xl text-white text-center  flex items-center justify-center w-32 h-32 p-10 bg-white border border-gray-200 drop-shadow-lg hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700 mx-auto my-4">
                     17
                 </h1>
-            <h1 className="rounded-full font-bold text-6xl text-white text-center  flex items-center justify-center w-32 h-32 p-10 bg-white border border-gray-200 drop-shadow-lg hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700 mx-auto my-4">
+            <h1 className="rounded font-bold text-6xl text-white text-center  flex items-center justify-center w-32 h-32 p-10 bg-white border border-gray-200 drop-shadow-lg hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700 mx-auto my-4">
                     18
                 </h1>
-            <h1 className="rounded-full font-bold text-6xl text-white text-center  flex items-center justify-center w-32 h-32 p-10 bg-white border border-gray-200 drop-shadow-lg hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700 mx-auto my-4">
+            <h1 className="rounded font-bold text-6xl text-white text-center  flex items-center justify-center w-32 h-32 p-10 bg-white border border-gray-200 drop-shadow-lg hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700 mx-auto my-4">
                     19
                 </h1>
 
@@ -37,7 +37,7 @@ export const Premios = () => {
                     Premios
                 </h1>
             </div>
-        <div className="flex flex-wrap flex-row max-w-full p-6 bg-white border border-gray-200 rounded-lg text-centered justify-center items-center">
+        <div className="flex flex-wrap flex-row max-w-full p-6 rounded-lg text-centered justify-center items-center">
             
             <div className="flex flex-wrap flex-col justify-center items-center m-10 block max-w-sm p-10 bg-white border border-gray-200 rounded-lg shadow hover:bg-red-100 dark:bg-red-700 dark:border-red-700 dark:hover:bg-red-700">
                 <div className="flex flex-wrap flex-row justify-center items-center">
