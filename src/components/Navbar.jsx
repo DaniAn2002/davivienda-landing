ce568ecfbdd614f4199c5582a253a63036e09bf0
Daniel An <cruzalexd1@gmail.com>
2024-02-21 20:01:42 -0600

    :art:refactor(app):change structure of navbar and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,41 @@
 export const Navbar = () => {
     return (
-        <nav className="z-10 sticky top-0 px-12 bg-color-nav navbar-font mb-10">
-            <ul className="flex justify-between">
-                <div className="flex justify-start">
-                    <li className="px-8 -mr-6"><img className="davivienda-navbar-img" src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/v1/davivienda-landing/uxszluory8yt2ilgqrgw" alt="" /></li>
-                    <li className="px-8 text-xl pt-4"><a href="">Home</a></li>
-                    <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/v1/davivienda-landing/zgx46rn03ikuckzdizdv" className="davivienda-navbar-icons" alt="" />
-                    <li className="px-8 text-xl pt-4"><a href="">Sobre Nosotros</a></li>
-                    <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/v1/davivienda-landing/ihnqgocjkdq4dgqhv1he" className="davivienda-navbar-icons-light" alt="" />
-                    <li className="px-8 text-xl pt-4"><a href="">Contactanos</a></li>
-                    <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/v1/davivienda-landing/w5ubkj9dxrcceuqejwfm" className="davivienda-navbar-icons-light" alt="" />
+
+        <div className="min-h-full">
+            <nav style={{ backgroundColor: 'rgba(214,35,41,255)' }}>
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div className="flex h-16 items-center justify-between">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <img className="h-14 w-14" src="https://pbs.twimg.com/profile_images/1511758253616738313/SiXehKkx_400x400.jpg" alt="Your Company" />
+                            </div>
+                            <div className="hidden md:block">
+                                <div className="ml-10 flex items-baseline space-x-4">
+
+                                    <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+                                    <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black rounded-md px-3 py-2 text-sm font-medium">Premios</a>
+                                    <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black rounded-md px-3 py-2 text-sm font-medium">Participantes</a>
+                                    <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black rounded-md px-3 py-2 text-sm font-medium">Hackaton</a>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
-                <div className="flex">
-                    <li><button className="px-8 text-xl py-2 bg-inscribete"><a href="">Registrate</a></button></li>
+
+                <div className="md:hidden" id="mobile-menu">
+                    <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                        <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
+                        <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black block rounded-md px-3 py-2 text-base font-medium">Premios</a>
+                        <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black block rounded-md px-3 py-2 text-base font-medium">Participantes</a>
+                        <a href="#" className="text-gray-300 hover:bg-zinc-50 hover:text-black block rounded-md px-3 py-2 text-base font-medium">Hackaton</a>
+                    </div>
                 </div>
-            </ul >
-        </nav >
+            </nav>
+
+
+        </div>
+
     )
 }
