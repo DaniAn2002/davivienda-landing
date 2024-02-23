8185d2792102414dab987fb26571495073127a0f
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-02-23 15:09:51 -0600

    :necktie:refactor(hero):change button size and hackatón word to English

--- src/components/MainHero.jsx
+++ src/components/MainHero.jsx
@@ -3,13 +3,13 @@ const MainHero = () => {
     <section id="home" className="flex flex-wrap justify-between items-center relative max-w-[1280px] mx-4 pt-20 md:justify-evenly md:mx-auto lg:justify-around">
       <div className="max-full text-center sm:max-w-[calc(270px+4vw)] md:max-w-[calc(350px+3vw)] md:text-start lg:max-w-[500px]">
         <h1 className="text-4xl block text-[#ED1C27] tracking-wide font-bold mb-4 sm:text-[calc(2.5rem+2vw)] md:text-[4.2rem]/[1] lg:text-[5.7rem]">
-          Hackatón Davivienda
+          Hackathon Davivienda
         </h1>
         <p className="text-2xl mb-2 text-pretty sm:text-3xl md:text-balance lg:text-4xl">
-          Primer Hackatón con jóvenes universitarios en <span className="text-blue-600">El Salvador</span>
+          Primer Hackathon con jóvenes universitarios en <span className="text-blue-600">El Salvador</span>
         </p>
-        <a href="#" className="block text-xs mb-5 hover:underline min-[425px]:text-sm hover:decoration-[#ED1C27] hover:text-[#ED1C27]">Powered by DaviPlata</a>
-        <a href="#" className="block rounded-3xl text-white font-bold text-lg bg-red-600 p-5 mb-5 w-fit mx-auto sm:text-2xl md:mx-0 hover:bg-rose-700 hover:shadow-xl hover:shadow-slate-950/40 hover:scale-105">Inscribase aquí</a>
+        <a href="#" className="block text-xs mb-5 sm:mb-6 hover:underline min-[425px]:text-sm hover:decoration-[#ED1C27] hover:text-[#ED1C27]">Powered by DaviPlata</a>
+        <a href="#" className="block rounded-3xl text-white font-bold text-xl bg-red-600 p-5 mb-4 w-fit mx-auto sm:text-2xl sm:p-6 md:text-3xl md:p-7 md:mx-0 lg:text-4xl lg:p-8 hover:bg-rose-700 hover:shadow-xl hover:shadow-slate-950/40 hover:scale-105">Inscríbete aquí</a>
       </div>
       <img src="https://res.cloudinary.com/dk2oxzxoo/image/upload/v1708385914/kodigo-Davivienda/hero-clean_jtgyno.png"
         alt="hero-img" className="w-full rounded-[10%] sm:max-w-[calc(270px+4vw)] md:max-w-[calc(350px+3vw)] lg:max-w-[500px]" />
