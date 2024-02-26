551e560a6eaf49f0c74b1053f95e291fdf8f17a6
Josue Orantes <daniel02josue@gmail.com>
2024-02-26 00:35:43 -0600

    :lipstick:style(perfilHackathon): modificar estilo del componente perfilHackathon

--- src/components/PerfilHackathon.jsx
+++ src/components/PerfilHackathon.jsx
@@ -5,10 +5,11 @@ export const PerfilHackathon = () => {
                 <div name="PerfilHackathon" className="container mx-auto rounded-xl shadow-md my-4">
                     <div className="bg-red-600 md:grid md:grid-flow-col md:grid-cols-2 justify-center gap-5 p-5 rounded-t-xl">
                         <div className="content-center tracking-wide m-auto">
-                            <p className="text-xl md:text-3xl lg:text-4xl text-center text-balance align-middle p-5 text-white font-medium">
-                                Si tienes entre 18 y 29 años y
-                                <span className="font-bold"> eres estudiante, egresado o graduado</span>,
-                                ¡acepta el desafío! Junto con tu equipo, <span className="font-extrabold"> podrían ganar hasta $3,000.00 dólares</span>.
+                            <p className="text-xl md:text-4xl lg:text-5xl text-center text-balance align-middle p-5 text-white font-medium">
+                                Si tienes entre <span className="text-cyan-900 bg-white rounded-full px-2">18</span> y <span className="text-cyan-900 bg-white rounded-full px-1">
+                                    29</span> años,
+                                ¡acepta el desafío! Junto con tu equipo, 
+                                <span className="font-extrabold"> podrían ganar hasta $3000 dólares</span>.
                             </p>
                         </div>
                         <div className="tracking-wide m-auto">
@@ -17,39 +18,60 @@ export const PerfilHackathon = () => {
                                 alt="hombre con laptop" />
                         </div>
                     </div>
-                    <div className="md:grid md:grid-flow-col md:grid-cols-3">
-                        <div className="tracking-wide pt-4">
-                            <img src="https://res.cloudinary.com/dbhtbf2uu/image/upload/v1708569620/Davivienda-landing/mtcz0ofpnmooedlfezlk.png"
-                                className="w-24 block mx-auto my-2"
-                                alt="Reto" />
-                            <span className="block font-bold text-2xl text-center mx-auto">
-                                El Reto
-                            </span>
-                            <p className="text-xl text-center text-balance font-medium pt-4 pb-9 px-[calc(1.5rem+5vw)] sm:px-16 md:px-4 xl:px-8">
+
+                    <div className="md:grid md:grid-flow-rows md:grid-cols-auto bg-gray-200 rounded-md">
+                        <div className="tracking-wide pt-4 lg:flex md:border-r md:border-l backdrop-blur-md bg-white/90 rounded-md m-1">
+                            <div className="lg:w-1/2 lg:my-5 lg:drop-shadow-md lg:p-1">
+                                <img src="https://res.cloudinary.com/dbhtbf2uu/image/upload/v1708923931/Davivienda-landing/j3o6ew7n5lcpopjzddp8.png"
+                                    className="w-2/6 md:w-24 lg:w-40 block mx-auto my-2"
+                                    alt="Reto" />
+                                <span className="block font-bold text-2xl text-center mx-auto">
+                                    El Reto
+                                </span>
+                            </div>
+                            <p className="lg:w-1/2 text-center sm:text-center lg:text-left sm:text-xl text-balance font-medium lg:text-2xl pt-10 pb-9 px-[calc(1.5rem+5vw)] sm:px-16 md:px-4 xl:px-3 lg:my-5">
                                 Diseñar la oferta de valor y experiencia para los jóvenes universitarios que fortalezca el ecosistema digital
                                 de pagos en la universidades.
                             </p>
                         </div>
-                        <div className="tracking-wide pt-4 md:border-r md:border-l md:border-slate-200">
-                            <img src="https://res.cloudinary.com/dbhtbf2uu/image/upload/v1708475022/Davivienda-landing/tj9oybrpm45ywkuraa9h.png"
-                                className="w-24 block mx-auto my-2"
-                                alt="Participantes" />
-                            <span className="block font-bold text-2xl text-center mx-auto">
-                                ¿Quiénes participan?
-                            </span>
-                            <p className="text-xl text-center text-balance font-medium pt-4 pb-9 px-[calc(1.5rem+5vw)] sm:px-20 md:px-2">
-                                Equipos de 2 a 5 personas conformados por estudiantes, egresados y graduados de Universidades de El Salvador,
-                                de cualquier carrera afín a tecnología, administración de empresas y diseño.
-                            </p>
+
+                        <div className="lg:flex tracking-wide p-4 md:border-r md:border-l backdrop-blur-md bg-white/90 rounded-md m-1 h-auto">
+                            <div className="lg:w-1/2 lg:my-5 lg:drop-shadow-md lg:p-1">
+                                <img src="https://res.cloudinary.com/dbhtbf2uu/image/upload/v1708923960/Davivienda-landing/m15568pq46o3vyhkqv0x.png"
+                                    className="w-2/6 md:w-24 lg:w-40 block mx-auto my-2"
+                                    alt="Participantes" />
+                                <span className="block font-bold text-2xl text-center mx-auto">
+                                    ¿Quiénes participan?
+                                </span>
+                            </div>
+                            <div className="block lg:w-1/2 ">
+                                <p className="block text-xl sm:text-center lg:text-left text-balance font-medium pt-2 pb-4 px-[calc(1.5rem+5vw)] sm:px-20 md:px-2">
+                                    Equipos de 2 a 5 personas conformados por estudiantes, egresados y graduados de Universidades de El Salvador,
+                                    de cualquier carrera afín a tecnología, administración de empresas y diseño.
+                                </p>
+                                <h2 className="text-xl font-bold pt-2 px-[calc(1.5rem+5vw)] sm:px-20 md:px-2">Carreras</h2>
+                                <ul className="mx-auto font-medium text-xl text-wrap pt-2 pb-4 px-[calc(1.5rem+5vw)] sm:px-20 md:px-2">
+                                    <li className="py-1 tracking-wide">- Lic. En Innovación y Transformación Digital</li>
+                                    <li className="py-1 tracking-wide">- Ing. En Software y Negocios Digitales</li>
+                                    <li className="py-1 tracking-wide">- Ingeniería Industrial, Informática, en Sistemas y de Negocios</li>
+                                    <li className="py-1 tracking-wide">- Diseño Estratégico</li>
+                                    <li className="py-1 tracking-wide">- Comunicación y Estrategia Digital</li>
+                                    <li className="py-1 tracking-wide">- Administración de Empresas y Mercadeo</li>
+                                    <li className="py-1 tracking-wide">- Ciencia de Datos (Desarrolladores / programadores)</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="tracking-wide pt-4">
-                            <img src="https://res.cloudinary.com/dbhtbf2uu/image/upload/v1708475686/Davivienda-landing/nxtqasvnvf82rbbqt904.png"
-                                className="w-24 block mx-auto my-2"
+
+                        <div className="tracking-wide pt-4 lg:flex md:border-r md:border-l backdrop-blur-md bg-white/90 rounded-xl m-1">
+                            <div className="lg:w-1/2 lg:my-5 lg:drop-shadow-md lg:p-1">
+                            <img src="https://res.cloudinary.com/dbhtbf2uu/image/upload/v1708923984/Davivienda-landing/fynovvllmkwocdpobkfw.png"
+                                className="w-2/6 md:w-24 lg:w-40 block mx-auto my-2"
                                 alt="Mockup" />
                             <span className="block font-bold text-2xl text-center mx-auto">
                                 ¿Qué se debe entregar?
                             </span>
-                            <p className="text-xl text-center text-balance font-medium pt-4 pb-9 px-[calc(1.5rem+5vw)] sm:px-16 md:px-4 xl:px-7">
+                            </div>
+                            <p className="lg:w-1/2 text-center sm:text-xl lg:text-2xl sm:text-center lg:text-left text-balance font-medium pt-8 pb-9 px-[calc(1.5rem+5vw)] sm:px-16 md:px-4 xl:px-3">
                                 Durante el evento de la Hackathon los equipos deben conceptualizar, prototipar a nivel de mockups y desarrollar
                                 la nueva oferta de valor dentro de Daviplata y su código de desarrollo.
                             </p>
