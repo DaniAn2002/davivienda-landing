43f2ac8912e6e0017dc42c499750de67f5abcf3b
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-03-01 00:27:06 -0600

    :lipstick:refactor(fechas):delete a section in the timeline and change bg colors

--- src/components/Fechas.jsx
+++ src/components/Fechas.jsx
@@ -3,18 +3,18 @@ const Fechas = () => {
         <section>
             <h1 id="fechas" className="text-center font-bold text-6xl pt-20 pb-8">Fechas</h1>
             <div className="bg-[#DC2626] lg:p-4">
-                <section className="lg:border-4 lg:border-davivienda-smoke lg:rounded-2xl lg:p-1 text-white max-w-screen-lg mx-auto">
-                    <div className="flex flex-col md:flex-row justify-between items-center max-w-screen-xl">
-                        <div className="hidden md:block md:w-1/2 mb-2">
+                <section className="p-1 lg:border-4 lg:border-davivienda-smoke lg:rounded-2xl text-white md:max-w-screen-md lg:max-w-[945px] mx-auto">
+                    <div className="flex flex-col md:flex-row justify-center lg:justify-between items-center max-w-screen-xl ">
+                        <div className="hidden md:block w-fit mb-2">
                             <img src="https://res.cloudinary.com/dyhftwfrw/image/upload/v1708971274/davi/xeroq7cuhoqj4oknekmr.jpg" alt="Hero Image"
-                                className="md:angled-mask drop-shadow-xl md:w-[500px] sm:w-[300px] max-h-[636px] border-2 border-davivienda-smoke rounded-tl-2xl" />
+                                className="angled-mask drop-shadow-xl border-2 border-davivienda-smoke h-[575px] w-[425px] rounded-tl-2xl" />
                         </div>
-                        <div className="mb-2 mx-2 md:w-1/2 2xl:my-0">
-                            <div className="relative grid grid-cols-1 mx-auto text-cyan-950 w-[calc(290px+8vw)] min-[400px]:w-[calc(375px+5vw)] md:w-full md:max-w-[400px]">
-                                <img className="absolute self-center justify-self-center h-[81%] pb-4"
+                        <div className="mb-2 mx-2 md:w-[400px] 2xl:my-0">
+                            <div className="relative grid grid-cols-1 mx-auto text-davivienda-smoke w-[calc(290px+8vw)] min-[400px]:w-[calc(375px+5vw)] md:w-full md:max-w-[400px]">
+                                <img className="absolute self-center justify-self-center h-[76%] pb-4"
                                     src="https://res.cloudinary.com/dk2oxzxoo/image/upload/v1709031663/kodigo-Davivienda/Line_clean_sfzc5d.png" alt="Time line" />
                                 <div className="grid items-center mt-2">
-                                    <div className="bg-davivienda-smoke rounded-2xl drop-shadow-xl w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px]">
+                                    <div className="bg-davivienda-red border border-davivienda-smoke rounded-2xl drop-shadow-xl shadow shadow-davivienda-smoke w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px]">
                                         <h2 className="text-lg font-bold underline md:text-xl">Convocatoria</h2>
                                         <span className="block text-sm md:text-base">Virtual</span>
                                         <p className="text-sm italic md:text-base">26 FEB - 17 ABR</p>
@@ -24,7 +24,7 @@ const Fechas = () => {
                                     </div>
                                 </div>
                                 <div className="grid items-center">
-                                    <div className="bg-davivienda-smoke rounded-2xl drop-shadow-xl w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px] justify-self-end">
+                                    <div className="bg-davivienda-red border border-davivienda-smoke rounded-2xl drop-shadow-xl shadow shadow-davivienda-smoke w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px] justify-self-end">
                                         <h2 className="text-lg font-bold underline md:text-xl">Kickoff</h2>
                                         <span className="block text-sm md:text-base">Virtual</span>
                                         <p className="text-sm italic md:text-base">22 ABR</p>
@@ -34,7 +34,7 @@ const Fechas = () => {
                                     </div>
                                 </div>
                                 <div className="grid items-center">
-                                    <div className="bg-davivienda-smoke rounded-2xl drop-shadow-xl w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px]">
+                                    <div className="bg-davivienda-red border border-davivienda-smoke rounded-2xl drop-shadow-xl shadow shadow-davivienda-smoke w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px]">
                                         <h2 className="text-lg font-bold underline md:text-xl">Sesión Q&A</h2>
                                         <span className="block text-sm md:text-base">Virtual</span>
                                         <p className="text-sm italic md:text-base">25 ABR</p>
@@ -42,23 +42,13 @@ const Fechas = () => {
                                     </div>
                                     <div className="absolute w-[18px] h-[18px] bg-[#F78E20] border border-davivienda-smoke rounded-full justify-self-center">
                                     </div>
-                                </div>
+                                </div>             
                                 <div className="grid items-center">
-                                    <div className="bg-davivienda-smoke rounded-2xl drop-shadow-xl w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px] justify-self-end">
-                                        <h2 className="text-lg font-bold underline md:text-xl">Primer Filtro</h2>
-                                        <span className="block text-sm md:text-base">Virtual</span>
-                                        <p className="text-sm italic md:text-base">13 - 14 MAY</p>
-                                        <p className="font-semibold">Selección <span className="block">de equipos</span></p>
-                                    </div>
-                                    <div className="absolute w-[18px] h-[18px] bg-[#F78E20] border border-davivienda-smoke rounded-full justify-self-center">
-                                    </div>
-                                </div>
-                                <div className="grid items-center">
-                                    <div className="bg-davivienda-smoke rounded-2xl drop-shadow-xl w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px]">
+                                    <div className="bg-davivienda-red border border-davivienda-smoke rounded-2xl drop-shadow-xl shadow shadow-davivienda-smoke w-[calc(128px+5vw)] p-2 md:w-[165px] lg:w-[181px] justify-self-end">
                                         <h2 className="text-lg font-bold underline lg:text-xl">¡Evento principal!</h2>
                                         <span className="block text-sm md:text-base">Presencial</span>
                                         <p className="text-sm mb-2 italic md:text-base">17 - 19 MAY</p>
-                                        <img className="w-9 mx-auto" src="https://res.cloudinary.com/dk2oxzxoo/image/upload/v1709028198/kodigo-Davivienda/arrow-big-down-lines-filled_xzdm1y.png" alt="Arrow" />
+                                        <img className="w-9 mx-auto" src="https://res.cloudinary.com/dk2oxzxoo/image/upload/v1709273082/kodigo-Davivienda/arrow-big-down-lines-filled_sxwg5p.png" alt="Arrow" />
                                     </div>
                                     <div className="absolute w-[18px] h-[18px] bg-[#F78E20] border border-davivienda-smoke rounded-full justify-self-center">
                                     </div>
@@ -67,23 +57,23 @@ const Fechas = () => {
                         </div>
                     </div>
                     <div className="bg-[#DC2626] p-1 pb-2 2xl:pt-1 max-w-screen-xl">
-                        <div className="bg-davivienda-smoke border border-davivienda-smoke rounded-[20px] drop-shadow-lg  w-full mx-auto p-4 max-w-screen-sm lg:max-w-screen-md">
-                            <div className=" text-cyan-950 rounded-xl mb-6 mx-2 2xl:mb-12">
+                        <div className="bg-davivienda-red border border-davivienda-smoke rounded-[20px] drop-shadow-lg shadow shadow-davivienda-smoke w-full mx-auto p-4 max-w-screen-sm lg:max-w-screen-md">
+                            <div className=" text-davivienda-smoke rounded-xl mb-6 mx-2 2xl:mb-12">
                                 <h2 className="text-4xl font-bold leading-tight mb-2 text-center drop-shadow-lg">
                                     ¡Tres días de intensa innovación!</h2>
                                 <p className="text-center text-balance text-xl font-bold mb-2">Tú y tu equipo reunidos <span className="underline">presencialmente</span> para competir y compartir con la comunidad.</p>
                             </div>
                             <div className="flex flex-wrap justify-center gap-3 items-center">
-                                <h1 className="bg-[url('/calendar.svg')] rounded font-bold text-6xl text-cyan-950 text-center flex items-center justify-center w-32 h-32 pt-4 drop-shadow-lg md:w-28 md:h-28 lg:w-32 lg:h-32">
+                                <h1 className="bg-[url('/calendar.svg')] rounded font-bold text-6xl text-davivienda-smoke text-center flex items-center justify-center w-32 h-32 pt-4 drop-shadow-lg md:w-28 md:h-28 lg:w-32 lg:h-32">
                                     17
                                 </h1>
-                                <h1 className="bg-[url('/calendar.svg')] rounded font-bold text-6xl text-cyan-950 text-center flex items-center justify-center w-32 h-32 pt-4 drop-shadow-lg md:w-28 md:h-28 lg:w-32 lg:h-32">
+                                <h1 className="bg-[url('/calendar.svg')] rounded font-bold text-6xl text-davivienda-smoke text-center flex items-center justify-center w-32 h-32 pt-4 drop-shadow-lg md:w-28 md:h-28 lg:w-32 lg:h-32">
                                     18
                                 </h1>
-                                <h1 className="bg-[url('/calendar.svg')] rounded font-bold text-6xl text-cyan-950 text-center flex items-center justify-center w-32 h-32 pt-4 drop-shadow-lg md:w-28 md:h-28 lg:w-32 lg:h-32">
+                                <h1 className="bg-[url('/calendar.svg')] rounded font-bold text-6xl text-davivienda-smoke text-center flex items-center justify-center w-32 h-32 pt-4 drop-shadow-lg md:w-28 md:h-28 lg:w-32 lg:h-32">
                                     19
                                 </h1>
-                                <h1 className="rounded-full font-bold text-6xl text-center text-cyan-950 drop-shadow-lg p-4">
+                                <h1 className="rounded-full font-bold text-6xl text-center text-davivienda-smoke drop-shadow-lg p-4">
                                     de Mayo
                                 </h1>
                             </div>
