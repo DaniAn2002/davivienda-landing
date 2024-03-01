c29f87b238a84131f40b3145e7bed6a7390a4568
Jonathan Carranza <jhoncarranza1@outlook.es>
2024-03-01 13:33:47 -0600

    creo la pagina Authors.jsx y la carpeta Pages

---
+++ src/Pages/Authors.jsx
@@ -0,0 +1,109 @@
+import { Footer } from "../components/Footer"
+import { Navbar } from "../components/Navbar"
+
+export const Authors = () => {
+  return (
+    <>
+      
+  <Navbar/> 
+    <section className="h-20"></section>
+    <div className="h-auto w-full my-8">
+
+      <div className="grid grid-cols-3 gap-x-10 gap-y-10 px-10 py-10 ">
+
+
+        <div className="grid grid-cols-1 bg-white rounded-md">
+          <div className="flex justify-center items-center px-8">
+          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
+          </div>
+          
+          <div className="p-4">
+              <h2 className="">Participante 1</h2>
+              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
+              <p className="text-xs mt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
+              </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 bg-white rounded-md">
+          <div className="flex justify-center items-center px-8">
+          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
+          </div>
+          
+          <div className="p-4">
+              <h2 className="">Participante 1</h2>
+              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
+              <p className="text-xs mt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
+              </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 bg-white rounded-md">
+          <div className="flex justify-center items-center px-8">
+          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
+          </div>
+          
+          <div className="p-4">
+              <h2 className="">Participante 1</h2>
+              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
+              <p className="text-xs mt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
+              </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 bg-white rounded-md">
+          <div className="flex justify-center items-center px-8">
+          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
+          </div>
+          
+          <div className="p-4">
+              <h2 className="">Participante 1</h2>
+              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
+              <p className="text-xs mt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
+              </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 bg-white rounded-md">
+          <div className="flex justify-center items-center px-8">
+          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
+          </div>
+          
+          <div className="p-4">
+              <h2 className="">Participante 1</h2>
+              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
+              <p className="text-xs mt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
+              </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 bg-white rounded-md">
+          <div className="flex justify-center items-center px-8">
+          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
+          </div>
+          
+          <div className="p-4">
+              <h2 className="">Participante 1</h2>
+              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
+              <p className="text-xs mt-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
+              </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+ 
+    <section className="h-10 bg-transparent"></section>
+   
+    <Footer/>
+
+    </>
+  )
+}
+
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,6 +11,8 @@ export const Footer = () => {
                             <a href="https://www.daviplata.com/wps/portal/daviplata" target="_blank" className="flex py-6 items-center">
                                 <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/v1/davivienda-landing/yzvycgmjnmw7cophmszr" style={{ height: '3.75rem' }} className="me-3" alt="Logo DaviPlata" />
                             </a>
+
+                            <a href="../Pages/Authors.jsx" className="font-semibold text-davivienda-smoke uppercase">Authors</a>
                         </div>
 
                         <div className="grid grid-cols-2 gap-8 sm:gap-12 sm:grid-cols-4">
