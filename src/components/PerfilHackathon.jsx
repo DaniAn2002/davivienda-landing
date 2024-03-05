0c31424cdbc27f43916256c37175a7c58548a878
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-03-05 14:39:40 -0600

    :recycle:refactor:(premios):change the prize rank

--- src/components/PerfilHackathon.jsx
+++ src/components/PerfilHackathon.jsx
@@ -9,7 +9,7 @@ export const PerfilHackathon = () => {
                                 Si tienes entre <span className="font-bold">18</span> y <span className="font-bold">
                                     29</span> años,
                                 ¡acepta el desafío! Junto con tu equipo,
-                                <span className="font-extrabold"> podrían ganar hasta $3,000 dólares</span>.
+                                <span className="font-extrabold"> podrían ganar hasta $5,000 dólares</span>.
                             </p>
                         </div>
                         <div className="rounded-[40px] bg-davivienda-smoke w-fit h-fit my-5 mx-auto p-2">
