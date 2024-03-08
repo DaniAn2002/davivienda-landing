f6ac7e29279cbc6c6daf9afa5f3699d5c1389cff
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2024-03-07 21:34:38 -0600

    kodiAuthors

--- src/pages/Authors.jsx
+++ src/pages/Authors.jsx
@@ -1,107 +1,43 @@
 import { Footer } from "../components/Footer"
 import { Navbar } from "../components/Navbar"
-
+import kodiAuthors from "./KodiAuthors"
 export const Authors = () => {
+
+
+
   return (
     <>
-      
-  <Navbar/> 
-    <section className="h-20"></section>
-    <div className="h-auto w-full my-8">
 
-      <div className="grid grid-cols-3 gap-x-10 gap-y-10 px-10 py-10 ">
+      <Navbar />
+      <section className="flex flex-wrap justify-between  relative max-w-[1280px] mx-4 pt-20 md:justify-evenly md:mx-auto lg:justify-arounditems-center me-4">
+        {kodiAuthors.map((author, index) => (
+          <a key={index} href={author.github} className="group relative bg-black gap-2 my-2">
+            <img
+              alt={author.name}
+              src={author.img}
+              className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+            />
 
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium uppercase tracking-widest text-red-500">Developer</p>
 
-        <div className="grid grid-cols-1 bg-white rounded-md">
-          <div className="flex justify-center items-center px-8">
-          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
-          </div>
-          
-          <div className="p-4">
-              <h2 className="">Participante 1</h2>
-              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
-              <p className="text-xs mt-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
-              </p>
-          </div>
-        </div>
+              <p className="text-xl font-bold text-white sm:text-2xl">{author.name}</p>
 
-        <div className="grid grid-cols-1 bg-white rounded-md">
-          <div className="flex justify-center items-center px-8">
-          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
-          </div>
-          
-          <div className="p-4">
-              <h2 className="">Participante 1</h2>
-              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
-              <p className="text-xs mt-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
-              </p>
-          </div>
-        </div>
+              <div className="mt-32 sm:mt-48 lg:mt-64">
+                <div
+                  className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                >
+                  <p className="text-sm text-white">
+                    Kodigo
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
 
-        <div className="grid grid-cols-1 bg-white rounded-md">
-          <div className="flex justify-center items-center px-8">
-          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
-          </div>
-          
-          <div className="p-4">
-              <h2 className="">Participante 1</h2>
-              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
-              <p className="text-xs mt-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
-              </p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 bg-white rounded-md">
-          <div className="flex justify-center items-center px-8">
-          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
-          </div>
-          
-          <div className="p-4">
-              <h2 className="">Participante 1</h2>
-              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
-              <p className="text-xs mt-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
-              </p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 bg-white rounded-md">
-          <div className="flex justify-center items-center px-8">
-          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
-          </div>
-          
-          <div className="p-4">
-              <h2 className="">Participante 1</h2>
-              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
-              <p className="text-xs mt-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
-              </p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 bg-white rounded-md">
-          <div className="flex justify-center items-center px-8">
-          <img className="rounded-md rounded-b-none h-80 object-cover" src="https://res.cloudinary.com/das3fcpca/image/upload/v1696986492/Kodigo/hkfzcpoweyz8ec0qliom.jpg" alt="" />
-          </div>
-          
-          <div className="p-4">
-              <h2 className="">Participante 1</h2>
-              <button className="text-white px-2 py-1 rounded-full text-xs">hola</button>
-              <p className="text-xs mt-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illum, consequatur iure, deleniti possimus ducimus officia aperiam nulla sunt placeat iusto atque. Officia fuga nesciunt aliquam ullam magnam accusamus autem.
-              </p>
-          </div>
-        </div>
-      </div>
-      
-    </div>
- 
-    <section className="h-10 bg-transparent"></section>
-   
-    <Footer/>
+        ))}
+      </section>
+      <Footer />
 
     </>
   )
---
+++ src/pages/KodiAuthors.jsx
@@ -0,0 +1,40 @@
+const kodiAuthors = [
+    {
+        'name': 'Joaquin Juarez',
+        'img': 'https://avatars.githubusercontent.com/u/65634361?v=4',
+        'linkedin': '',
+        'github': 'https://github.com/jacubiam'
+    },
+    {
+        'name': 'Daniel An',
+        'img': 'https://avatars.githubusercontent.com/u/118091765?v=4',
+        'linkedin': '',
+        'github': 'https://github.com/DaniAn2002'
+    },
+    {
+        'name': 'Roberto Alferes',
+        'img': 'https://avatars.githubusercontent.com/u/44125440?v=4',
+        'linkedin': '',
+        'github': 'https://github.com/raag2018'
+    },
+    {
+        'name': 'Daniel Orantes',
+        'img': 'https://avatars.githubusercontent.com/u/88408649?v=4',
+        'linkedin': '',
+        'github': 'https://github.com/Dartanex'
+    },
+    {
+        'name': 'Rudy Rivera',
+        'img': 'https://res.cloudinary.com/dyhftwfrw/image/upload/v1709866758/davi/gud9fobq8jq4qjnokf7j.jpg',
+        'linkedin': '',
+        'github': 'https://github.com/RudyRi'
+    },
+    {
+        'name': 'Jonathan Carranza',
+        'img': 'https://avatars.githubusercontent.com/u/73750673?v=4',
+        'linkedin': '',
+        'github': 'https://github.com/JonathanCarranza'
+    }
+];
+
+export default kodiAuthors;
