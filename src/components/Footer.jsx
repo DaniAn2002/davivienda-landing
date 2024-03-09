a53d7872e5deba615eb19f5a90d937c59a28db7d
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-03-08 20:19:12 -0600

    :sparkles:feat(footer):add new links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,17 +28,22 @@ export const Footer = () => {
                                 <ul className="text-gray-400 font-medium text-opacity-85">
                                     <li className="mb-4">
                                         <div className="flex flex-col">
-                                            <a target="_blank" href="https://www.misfinanzasencasa.com/seleccione-su-pais" className="hover:underline">Mis Finanzas en Casa</a>
+                                            <a target="_blank" href="https://www.davivienda.com.sv" className="hover:underline">Banco Davivienda</a>
                                         </div>
                                     </li>
                                     <li className="mb-4">
                                         <div className="flex flex-col">
-                                            <a target="_blank" href="https://www.davivienda.com.sv/#/seguros-personas" className="hover:underline">Seguros</a>
+                                            <a target="_blank" href="https://www.daviplata.sv" className="hover:underline">DaviPlata</a>
+                                        </div>
+                                    </li>
+                                    <li className="mb-4">
+                                        <div className="flex flex-col">
+                                            <a target="_blank" href="https://www.misfinanzasencasa.com/seleccione-su-pais" className="hover:underline">Mis Finanzas en Casa</a>
                                         </div>
                                     </li>
                                     <li>
                                         <div className="flex flex-col">
-                                            <a target="_blank" href="https://www.davivienda.com.sv/#/personas/activos" className="hover:underline">Trabaja con nosotros</a>
+                                            <a target="_blank" href="https://jobs.airavirtual.com/davivienda_filiales" className="hover:underline">Trabaja con nosotros</a>
                                         </div>
                                     </li>
                                 </ul>
@@ -49,15 +54,8 @@ export const Footer = () => {
                                 <ul className="text-gray-400 font-medium text-opacity-85">
                                     <li className="mb-4">
                                         <div className="flex flex-col">
-                                            <a href="">Centro de Contacto</a>
+                                            <h3 >Centro de Contacto</h3>
                                             <a className="hover:underline">2556-0000</a>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="flex flex-col">
-
-                                            <a href="">Telemercadeo</a>
-                                            <a className="hover:underline">2567-2000</a>
                                         </div>
                                     </li>
                                 </ul>
@@ -83,7 +81,7 @@ export const Footer = () => {
                                 <h2 className="mb-6 text-sm font-semibold text-davivienda-smoke uppercase">Legal</h2>
                                 <ul className="text-gray-400 font-medium text-opacity-85">
                                     <li className="mb-4">
-                                        <a href="#" className="hover:underline">Politicas de privacidad</a>
+                                        <a href="https://www.davivienda.com.sv/#/personas/personas_legal" className="hover:underline">Politicas de privacidad</a>
                                     </li>
                                     <li>
                                         <a href="#" className="hover:underline">Terminos &amp; Condiciones</a>
@@ -105,14 +103,14 @@ export const Footer = () => {
                                 <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                                     <path d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" />
                                 </svg>
-                                <span className="sr-only">Facebook page</span>
+                                <span className="sr-only text-white">Facebook page</span>
                             </a>
 
                             <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
                                 <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                                     <path d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" />
                                 </svg>
-                                <span className="sr-only">Twitter page</span>
+                                <span className="sr-only text-white">Twitter page</span>
                             </a>
                         </div>
                     </div>
